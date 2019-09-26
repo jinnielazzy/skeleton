5b7e52f9b01dbe1891c79f11b5d3cc8e594d3b35
jinhuachen1314 <jic212@ucsd.edu>
2019-09-26 12:06:48 -0700

    add skeleton

---
+++ app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hello World!!</h1>
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
